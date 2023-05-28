bcbb6fd220f98ccbb4f9eff485a77c2d984d4381
jin <raquim47@naver.com>
2023-05-28 21:43:40 +0900

    Add 2주차 문제

---
+++ 2주차/1.가까운수/problem.js
@@ -0,0 +1,10 @@
+// 가까운 수
+// https://school.programmers.co.kr/learn/courses/30/lessons/120890?language=javascript
+// * 이진 탐색으로 풀 것
+
+function solution(array, n) {
+
+}
+
+console.log(solution([3, 10, 28], 20)); //28
+console.log(solution([10, 11, 12], 13)); //12
---
+++ 2주차/4.징검다리/problem.js
@@ -0,0 +1,9 @@
+// 징검다리
+// https://school.programmers.co.kr/learn/courses/30/lessons/43236?language=javascript
+
+function solution(distance, rocks, n) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(solution(25, [2, 14, 11, 21, 17], 2)); //4
