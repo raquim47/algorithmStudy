b3f7b5278c5e757a10391cd5e865a83447ea1e87
JAEUKJEON <jojeon4515@gmail.com>
2023-06-17 10:38:52 +0900

    update 4주차

--- 4주차/3.최소직사각형/problem.js
+++ 4주차/3.최소직사각형/problem.js
@@ -2,22 +2,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/86491
 // * 정렬 알고리즘을 사용할 것
 
-function solution(sizes) {
-  let max_width = 0;
-  let max_heigth = 0;
-  sizes.forEach(arr => {
-    if (arr[0] < arr[1]) swap(arr, 0, 1);
-    if (arr[0] > max_width) max_width = arr[0];
-    if (arr[1] > max_heigth) max_heigth = arr[1];
-  });
-  return max_width*max_heigth;
-}
-
-function swap(arr, i, j) {
-    let value = arr[i];
-    arr[i] = arr[j];
-    arr[j] = value;
-}
+function solution(sizes) {}
 
 console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]])); // 4000
 console.log(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]])); //	120
