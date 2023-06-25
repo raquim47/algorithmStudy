29ef86a04687e737b18fe79e71e8fd25bfac482a
jin <raquim47@naver.com>
2023-06-25 16:08:55 +0900

    Add 6주차 문제

---
+++ 6주차/1.완주하지못한선수/problem.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript
+
+function solution(participant, completion) {
+  
+}
+
+console.log(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki'])); // "leo"
+console.log(
+  solution(
+    ['marina', 'josipa', 'nikola', 'vinko', 'filipa'],
+    ['josipa', 'filipa', 'marina', 'nikola']
+  )
+); //"vinko"
+console.log(
+  solution(['mislav', 'stanko', 'mislav', 'ana'], ['stanko', 'ana', 'mislav'])
+); //	"mislav"
---
+++ 6주차/3.의상/problem.js
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=javascript
+
+function solution(clothes) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(
+  solution([
+    ['yellow_hat', 'headgear'],
+    ['blue_sunglasses', 'eyewear'],
+    ['green_turban', 'headgear'],
+  ])
+); //	5
+console.log(
+  solution([
+    ['crow_mask', 'face'],
+    ['blue_sunglasses', 'face'],
+    ['smoky_makeup', 'face'],
+  ])
+); //	3
